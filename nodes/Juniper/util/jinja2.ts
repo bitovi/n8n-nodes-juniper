999c03f24cb9db3b87ecca3972aae5343633a1d7
Roye <roye@bitovi.com>
2025-04-04 14:35:47 -0400

    fixes

--- nodes/Juniper/util/jinja2.ts
+++ nodes/Juniper/util/jinja2.ts
@@ -8,33 +8,39 @@ function findLoopOpportunities(diff: JuniperDiff[]) {
     const key = existingKey ?? wildcardPath;
     const existingList = acc[key] ?? [];
     const subgroup = +path.join('.').substr(key.length + 1).split('.')[0];
-    return { ...acc, [key]: [...existingList, {path: path.join('.'), subgroup}] }
+    return { ...acc, [key]: [...existingList, { path: path.join('.'), subgroup }] }
   }, {} as Record<string, Array<{ path: string, subgroup: number }>>);
 }
 
-function convertToLoopBody(ast: JuniperNode, variables: Record<string, object>): JuniperNode {
+function convertToLoopBody(ast: JuniperNode, variables: Record<string, object>, reveredVariables: Record<string, object>, currPath: string): JuniperNode {
   const replaced = { ...ast };
 
   if (replaced.name === 'interface' && replaced.value) {
     replaced.value = replaced.value.split('.').map((value, index) => {
-      if (index === 0 && variables[value].toString() === 'interface.name') return `{{${variables[value]}}}`;
-      if (index === 1 && variables[value].toString() === 'interface.unit.name') return `{{${variables[value]}}}`;
+      if (index === 0 && reveredVariables[value].toString() === 'interface.name') return `{{${reveredVariables[value]}}}`;
+      if (index === 1 && reveredVariables[value].toString() === 'interface.unit.name') return `{{${reveredVariables[value]}}}`;
       return value;
     }).join('.');
   } else {
-    // Check and replace `name` and `value` if found in variables
-		if (replaced.name) {
-    	replaced.name = variables[replaced.name] ? `{{${variables[replaced.name]}}}` : replaced.name;
-		}
+		const value = _.get(variables, currPath);
 
-		if (replaced.value) {
-    	replaced.value = variables[replaced.value] ? `{{${variables[replaced.value]}}}` : replaced.value;
+    if (typeof value === 'string') {
+      replaced.value = `{{${currPath}}}`;
+    } else if (!currPath.startsWith('interface.')) {
+			// Check and replace `name` and `value` if found in variables
+			if (replaced.name) {
+				replaced.name = reveredVariables[replaced.name] ? `{{${reveredVariables[replaced.name]}}}` : replaced.name;
+			}
+
+			if (replaced.value) {
+				replaced.value = reveredVariables[replaced.value] ? `{{${reveredVariables[replaced.value]}}}` : replaced.value;
+			}
 		}
   }
 
   // Recurse into children
   if (Array.isArray(replaced.children)) {
-    replaced.children = replaced.children.map(child => convertToLoopBody(child, variables));
+    replaced.children = replaced.children.map(child => convertToLoopBody(child, variables, reveredVariables, `${currPath}.${child.name}`));
   }
 
   return replaced;
@@ -55,6 +61,7 @@ function flattenObjectReverse(obj: object, prefix = '', res: any = {}) {
 export function convertToJinja2Ast(ast: JuniperNode, diff: JuniperDiff[], variables: Record<string, object>) {
   return Object.entries(findLoopOpportunities(diff)).reduce((acc, [loopPath, changes]) => {
     const subgroups = [...new Set(changes.map(({ subgroup }) => subgroup))];
+		const currPath = _.get(acc, loopPath.split('.').slice(0, -1).join('.')).name;
     const original = _.get(acc, loopPath);
     _.set(acc, loopPath, original.reduce((acc2: JuniperNode[], child: JuniperNode, index: number) => {
       if (index === subgroups[0]) {
@@ -67,7 +74,7 @@ export function convertToJinja2Ast(ast: JuniperNode, diff: JuniperDiff[], variab
               value: null,
               children: []
             },
-            convertToLoopBody(original[subgroups[0]], flattenObjectReverse(variables)),
+            convertToLoopBody(original[subgroups[0]], variables, flattenObjectReverse(variables), currPath.replace('interfaces', 'interface')),
             {
               type: 'flag',
               name: `{% endfor %}`,
